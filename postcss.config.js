841be6c072868f98592c46cb7c577b00eb845f0f
Greg Moon <greg.moon@artivest.co>
2019-01-27 19:54:16 -0500

    add cssnano

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,11 @@
+const mode = process.env.NODE_ENV == 'production' ? 'production' : 'development';
+
+const plugins = [require('autoprefixer')];
+
+if (mode == 'production') {
+    plugins.push(require('cssnano'));
+}
+
 module.exports = {
-    plugins: [
-        require('autoprefixer')
-    ]
+    plugins
 }
